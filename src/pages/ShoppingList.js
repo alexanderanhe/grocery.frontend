87c317fa64394354941376d073aefa052422b97c
Alexander Angulo <alex.anguloh@gmail.com>
2023-09-02 23:23:56 -0600

    init

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 
 const getShoppingList = async (shoppingListId) => {
-  try {
-    // const req = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/shopping/${shoppingListId}`);
-    // return await req.json();
-    return;
-  } catch (e) {
-    console.error(e.message);
-  }
+  // try {
+  //   // const req = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/shopping/${shoppingListId}`);
+  //   // return await req.json();
+  //   return;
+  // } catch (e) {
+  //   console.error(e.message);
+  // }
 };
 
 export async function loader({ params }) {
@@ -17,7 +17,7 @@ export async function loader({ params }) {
 }
 
 export default function ShoppingList() {
-  const { shoppingList } = useLoaderData();
+  // const { shoppingList } = useLoaderData();
 
   return (
     <>
