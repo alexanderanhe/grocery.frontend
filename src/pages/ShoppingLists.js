13f62177044db7b4ae48f7dc04dcb917f06792a7
Alexander Angulo <alex.anguloh@gmail.com>
2023-09-06 13:56:17 -0600

    fix layout

--- src/pages/ShoppingLists.js
+++ src/pages/ShoppingLists.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useLoaderData, Link } from "react-router-dom";
 import { ChevronRightIcon } from '@heroicons/react/24/solid';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Layout from '../components/Layout';
 
 const getShoppingList = async () => {
